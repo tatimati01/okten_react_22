a880a26e946acd5322a1872d0fd46e3ce115ffcb
tatimati01 <tati.mati.work@gmail.com>
2022-09-29 14:37:56 +0100

    homework1 finished

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Simpsons from "./components/Simpsons/Simpsons";
 import Characters from "./components/Characters/Characters";
 
 function App() {
-    return (<div>
+    return (
+        <div>
 
             <Simpsons/>
             <Characters/>
